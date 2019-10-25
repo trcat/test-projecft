51d05ef92facc17bc94d6cc7f49f7c6fc9285c6e
Henry Hu <daydaybigking@sina.com>
2019-10-25 09:56:48 +0800

    添加文件

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import "../assets/style/theme/index.css";
+import ElementUI from "element-ui";
+import Vue from "vue";
+import "../scss/message.scss";
 
-Vue.config.productionTip = false
+Vue.use(ElementUI);
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+Vue.config.productionTip = false;
+
+
+
